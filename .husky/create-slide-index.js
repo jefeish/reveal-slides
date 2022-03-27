92b5e34e2056f6c813c0d2000c8fdf8a3e1e6a00
jefeish <jefeish@github.com>
2022-03-27 16:32:33 +0000

    test

--- .husky/create-slide-index.js
+++ .husky/create-slide-index.js
@@ -5,33 +5,39 @@ console.log('Creating slide index...');
 const fs = require('fs');
 const util = require('util');
 const directory = '../docs/slides'
-let filesNames = [];
 let slides = {}
-let index = 0
+
 // Function to get current filenames in a directory
 fs.readdir(directory, (err, files) => {
     if (err)
         console.log(err);
     else {
         files.forEach(file => {
-            // index += 1
+            console.log('file: ' + file)
             const pageNumber = file.split('-')[0]
+
             if (pageNumber.includes('_')) {
                 const slide = pageNumber.split('_')[0]
                 const subSlide = pageNumber.split('_')[1]
-                index = slide
-                slides[slide].push(file);
+                console.log('       a: ' + slides[slide])
+
+                if (slides[slide].constructor === Array) {
+                    slides[slide] = slides[slide].push(file)
+                    console.log('       x: ' + file)
+
+                }
+                else {
+                    slides[slide] = [file]
+                }
+               
             } else {
                 slides[pageNumber] = file;
             }
-            
-            console.log(file);
-            filesNames.push(file);
-            // slides[index] = file;
         })
     }
-    console.log('filesNames:' + util.inspect(filesNames));
+    // console.log('filesNames:' + util.inspect(filesNames));
     console.log('slides:' + util.inspect(slides));
+    // console.log('slides:' + JSON.stringify(slides));
 })
 
 
